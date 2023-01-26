df2516aa8e07a8f6cc568f39c32e4c4765e40efa
Metroscript <122935820+Metroscript@users.noreply.github.com>
2023-01-26 07:04:06 +0000

    Update canary.sh

--- canary.sh
+++ canary.sh
@@ -6,10 +6,10 @@ sudo echo '/swapfile none swap 0 0' | sudo tee -a /etc/fstab
 sudo mount -a
 sudo swapon -a
 free -m
-sudo sed -i 's/#resume=/resume=/' /boot/loader/entries/arch.conf
+sudo sed -i 's/#resume=/resume=/' /efi/loader/entries/arch.conf
 sudo sed -i -e 's/#Color/Color/' -i -e "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 sudo mv archpkgs/mirrorlist /etc/pacman.d/mirrorlist
-sudo pacman -Sy --needed wayland mesa pipewire{,-{jack,pulse,alsa}} wireplumber plasma-{wayland-session,nm,desktop,pa,firewall,disks,thunderbolt,vault,systemmonitor} kde{plasma-addons,-gtk-config} k{gamma5,infocenter,screen,screenlocker,menuedit,wayland-integration,eepassxc,pipewire} powerdevil gimp discover sddm{,-kcm} nemo{,-fileroller,-share} gparted qbittorrent rhythmbox lollypop x{ed,reader,fce4-{terminal,screenshooter}} libreoffice-still gnome-calculator mpv print-manager simple-scan vulkan-{radeon,icd-loader} wine gnutls lib{pulse,32-{gnutls,libpulse,pipewire{,-jack},mesa,vulkan-{radeon,icd-loader}}} steam flatpak xdg-desktop-portal-kde phonon-qt5-gstreamer gnu-free-fonts ttf-ubuntu-font-family rust yt-dlp atomicparsley virtualbox{,-host-modules-arch} firewalld neofetch
+sudo pacman -Sy --needed linux-lts wayland mesa pipewire{,-{jack,pulse,alsa}} wireplumber plasma-{wayland-session,nm,desktop,pa,firewall,disks,thunderbolt,vault,systemmonitor} kde{plasma-addons,-gtk-config} k{gamma5,infocenter,screen,screenlocker,menuedit,wayland-integration,eepassxc,pipewire} powerdevil gimp discover sddm{,-kcm} nemo{,-fileroller,-share} gparted qbittorrent rhythmbox lollypop x{ed,reader,fce4-{terminal,screenshooter}} libreoffice-still gnome-calculator mpv print-manager simple-scan vulkan-{radeon,icd-loader} wine gnutls lib{pulse,32-{gnutls,libpulse,pipewire{,-jack},mesa,vulkan-{radeon,icd-loader}}} steam flatpak xdg-desktop-portal-kde phonon-qt5-gstreamer gnu-free-fonts ttf-ubuntu-font-family rust yt-dlp atomicparsley virtualbox{,-host-modules-arch} firewalld neofetch
 git clone --depth=1 https://aur.archlinux.org/grapejuice-git.git
 cd grapejuice-git
 makepkg -si --noconfirm
@@ -21,9 +21,11 @@ cd
 yay -S --noconfirm librewolf-bin popsicle-git xviewer timeshift plymouth-git downgrade
 sudo sed -i -e 's/udev/udev plymouth/g' -i -e 's/encrypt/plymouth-encrypt/g' /etc/mkinitcpio.conf
 sudo mkinitcpio -P linux
-sudo sed -i 's/splash/splash vt.global_cursor_default=0' /boot/loader/entries/arch.conf
+sudo sed -i 's/splash/splash vt.global_cursor_default=0' /efi/loader/entries/arch.conf
 sudo cp /usr/share/plymouth/arch-logo.png /usr/share/plymouth/themes/spinner/watermark.png
 sudo sed -i 's/WatermarkVerticalAlignment=0.96/WatermarkVerticalAlignment=0.5' /usr/share/plymouth/themes/spinner/spinner.plymouth
+sudo cp /efi/loader/entries/arch.conf /efi/loader/entries/lts.conf
+sudo sed -i -e 's/vmlinuz-linux/vmlinuz-linux-lts' -i -e 's/initramfs-linux/initramfs-linux-lts' /efi/loader/entries/lts.conf
 gsettings set org.cinnamon.desktop.privacy remember-recent-files false
 gsettings set org.cinnamon.desktop.default-applications.terminal exec xfce4-terminal
 sudo mv archpkgs/sha256sum.nemo_action /usr/share/nemo/actions
