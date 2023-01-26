cf2529c9f67f06c0a4e6b42b7e65580a3fe04d39
Metroscript <122935820+Metroscript@users.noreply.github.com>
2023-01-26 10:00:22 +0000

    Update canary.sh

--- canary.sh
+++ canary.sh
@@ -1,3 +1,4 @@
+#Make Swapfile
 sudo dd if=/dev/zero of=/swapfile bs=1M count=2048 status=progress
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
@@ -6,6 +7,7 @@ sudo echo '/swapfile none swap 0 0' | sudo tee -a /etc/fstab
 sudo mount -a
 sudo swapon -a
 free -m
+
 sudo sed -i 's/#resume=/resume=/' /boot/loader/entries/arch.conf
 sudo sed -i -e 's/#Color/Color/' -i -e "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 sudo mv archpkgs/mirrorlist /etc/pacman.d/mirrorlist
@@ -19,13 +21,22 @@ cd yay
 makepkg -si --noconfirm
 cd
 yay -S --noconfirm librewolf-bin popsicle-git xviewer timeshift plymouth-git downgrade
+
+#Configure Plymouth
 sudo sed -i -e 's/udev/udev plymouth/g' -i -e 's/encrypt/plymouth-encrypt/g' /etc/mkinitcpio.conf
 sudo mkinitcpio -P linux
 sudo sed -i 's/splash/splash vt.global_cursor_default=0/' /boot/loader/entries/arch.conf
 sudo cp /usr/share/plymouth/arch-logo.png /usr/share/plymouth/themes/spinner/watermark.png
 sudo sed -i 's/WatermarkVerticalAlignment=0.96/WatermarkVerticalAlignment=0.5/' /usr/share/plymouth/themes/spinner/spinner.plymouth
+
+#Kernel fallbacks
+sudo cp /boot/loader/entries/arch.conf /boot/loader/entries/arch_fallback.conf
+sudo sed -i -e 's/Arch Linux/Arch Linux Fallback/' -i -e 's/initramfs-linux/initramfs-linux-fallback/' /boot/loader/entries/arch-fallback.conf
 sudo cp /boot/loader/entries/arch.conf /boot/loader/entries/lts.conf
 sudo sed -i -e 's/Arch Linux/Arch Linux-LTS/' -i -e 's/vmlinuz-linux/vmlinuz-linux-lts/' -i -e 's/initramfs-linux/initramfs-linux-lts/' /boot/loader/entries/lts.conf
+sudo cp /boot/loader/entries/lts.conf /boot/loader/entries/lts-fallback.conf
+sudo sed -i -e 's/Arch Linux/Arch Linux-LTS Fallback/' -i -e 's/initramfs-linux/initramfs-linux-lts-fallback/' /boot/loader/entries/lts-fallback.conf
+
 gsettings set org.cinnamon.desktop.privacy remember-recent-files false
 gsettings set org.cinnamon.desktop.default-applications.terminal exec xfce4-terminal
 sudo mv archpkgs/sha256sum.nemo_action /usr/share/nemo/actions
