e84f811a1ad5758e8ab720910e08813b9ba22fce
Metroscript <122935820+Metroscript@users.noreply.github.com>
2023-02-19 15:50:03 +1100

    Update canary.sh

--- canary.sh
+++ canary.sh
@@ -12,7 +12,7 @@ sudo sed -i 's/#resume=/resume=/' /boot/loader/entries/arch.conf
 #Install Packages
 sudo sed -i -e 's/#Color/Color/' -i -e "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 sudo mv archpkgs/mirrorlist /etc/pacman.d/mirrorlist
-sudo pacman -Syu --needed linux-{headers,lts{,-headers}} wayland mesa pipewire{,-{jack,pulse,alsa}} wireplumber plasma-{wayland-session,nm,desktop,pa,firewall,disks,thunderbolt,vault,systemmonitor} k{gamma5,infocenter,screen,screenlocker,menuedit,wayland-integration,eepassxc,pipewire,de{plasma-addons,-gtk-config}} powerdevil gimp discover sddm{,-kcm} nemo{,-fileroller,-share} gparted qbittorrent rhythmbox lollypop x{ed,reader,fce4-terminal} spectacle libreoffice-still gnome-calculator mpv print-manager simple-scan vulkan-{radeon,icd-loader} wine{,-gecko} gnutls lutris vkd3d lib{pulse,mythes,va-mesa-driver,32-{gnutls,lib{pulse,va-mesa-driver},vkd3d,pipewire{,-jack},mesa,vulkan-{radeon,icd-loader}}} steam flatpak xdg-desktop-portal-kde phonon-qt5-gstreamer gnu-free-fonts vim{,-spell-en} ttf-{hack-nerd,ubuntu-font-family} rust hunspell{,-en_au} mythes-en yt-dlp cups{,-pdf} gutenprint foomatic-db-{engine,ppds,gutenprint-ppds} atomicparsley virtualbox{,-host-dkms} firewalld neofetch
+sudo pacman -Syu --needed linux-{headers,lts{,-headers}} wayland mesa pipewire{,-{jack,pulse,alsa}} wireplumber gwenview plasma-{wayland-session,nm,desktop,pa,firewall,disks,thunderbolt,vault,systemmonitor} k{gamma5,infocenter,screen,screenlocker,menuedit,wayland-integration,eepassxc,pipewire,de{plasma-addons,-gtk-config}} powerdevil gimp discover sddm{,-kcm} nemo{,-fileroller,-share} gparted qbittorrent rhythmbox lollypop x{ed,reader,fce4-terminal} spectacle libreoffice-still gnome-calculator mpv print-manager simple-scan vulkan-{radeon,icd-loader} wine{,-gecko} gnutls lutris vkd3d lib{pulse,mythes,va-mesa-driver,32-{gnutls,lib{pulse,va-mesa-driver},vkd3d,pipewire{,-jack},mesa,vulkan-{radeon,icd-loader}}} steam flatpak xdg-desktop-portal-kde phonon-qt5-gstreamer gnu-free-fonts vim{,-spell-en} ttf-{hack-nerd,ubuntu-font-family} rust hunspell{,-en_au} mythes-en yt-dlp cups{,-pdf} gutenprint foomatic-db-{engine,ppds,gutenprint-ppds} atomicparsley virtualbox{,-host-dkms} firewalld neofetch
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 git clone --depth=1 https://aur.archlinux.org/grapejuice-git.git
 cd grapejuice-git
@@ -22,12 +22,12 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si --noconfirm
 cd
-yay -S --noconfirm librewolf-bin popsicle-git xviewer timeshift downgrade
+yay -S --noconfirm librewolf-bin popsicle-git plymouth-git timeshift downgrade
 
 #Configure Plymouth
-#sudo sed -i -e 's/udev/udev plymouth/g' -i -e 's/encrypt/plymouth-encrypt/g' /etc/mkinitcpio.conf
-#sudo mkinitcpio -p linux
-#sudo sed -i 's/splash/splash vt.global_cursor_default=0/' /boot/loader/entries/arch.conf
+sudo sed -i -e 's/MODULES=()/MODULES=(amdgpu)/' -i -e 's/udev/udev plymouth/g' -i -e 's/encrypt/plymouth-encrypt/g' /etc/mkinitcpio.conf
+sudo mkinitcpio -p linux linux-lts
+sudo sed -i 's/splash/splash vt.global_cursor_default=0/' /boot/loader/entries/arch.conf
 #sudo cp /usr/share/plymouth/arch-logo.png /usr/share/plymouth/themes/spinner/watermark.png
 #sudo sed -i 's/WatermarkVerticalAlignment=0.96/WatermarkVerticalAlignment=.5/' /usr/share/plymouth/themes/spinner/spinner.plymouth
 #sudo plymouth-set-default-theme -R spinner
@@ -47,6 +47,6 @@ mv archpkgs/bashrc .bashrc
 mv archpkgs/yt-dlp.conf .config/
 mv arch pkgs/vimrc .vimrc
 sudo timedatectl set-timezone Australia/Sydney
-sudo systemctl enable systemd-timesyncd sddm cups
+sudo systemctl enable systemd-timesyncd sddm-plymouth cups
 systemctl --user --now enable wireplumber.service pipewire.service pipewire-pulse.service
 reboot
